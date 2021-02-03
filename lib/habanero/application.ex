8aa49d67897fb5ce42b20f81ea4a055e8ae62d90
Faheetah <Faheetah@users.noreply.github.com>
2021-02-03 16:28:34 -0600

    phx.new

---
+++ lib/habanero/application.ex
@@ -0,0 +1,32 @@
+defmodule Habanero.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      HabaneroWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Habanero.PubSub},
+      # Start the Endpoint (http/https)
+      HabaneroWeb.Endpoint
+      # Start a worker by calling: Habanero.Worker.start_link(arg)
+      # {Habanero.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Habanero.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    HabaneroWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
