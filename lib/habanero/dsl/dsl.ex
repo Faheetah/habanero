8db774829b11d4a7c25f99ad6a80983a13c4a25e
Faheetah <Faheetah@users.noreply.github.com>
2021-02-20 12:21:37 -0600

    Add sparse SDL module with module docs

---
+++ lib/habanero/dsl/dsl.ex
@@ -0,0 +1,8 @@
+defmodule DSL do
+  @moduledoc """
+  Constructs a DSL and returns a mapping of instructions to execute
+  """
+
+  @doc "Parses DSL text and returns a nested struct of all instructions"
+  def parse(text), do: text
+end
