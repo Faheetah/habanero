01742ed9f5138f48fe7e088daaca193abfc03aeb
Faheetah <Faheetah@users.noreply.github.com>
2021-03-07 09:01:27 -0600

    Add some high level functions

--- lib/habanero.ex
+++ lib/habanero.ex
@@ -1,3 +1,14 @@
 defmodule Habanero do
-  @moduledoc false
+  @moduledoc """
+  High level functions for managing the Habanero stack
+
+  Most of the functionality duplicates the Erlang init module, to provide more extensability
+  http://erlang.org/doc/man/init.html
+  """
+
+  @doc "Stop the Habanero instance"
+  def stop(), do: :init.stop()
+
+  @doc "Restart the Habanero instance"
+  def restart(), do: :init.restart()
 end
